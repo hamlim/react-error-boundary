aeda17d8552746991a6608d650c685ea4030be9b
Matt Hamlin <hamlim@outlook.com>
2018-04-03 20:47:14 -0400

    Cleaning up the package

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 export default class ErrorBoundary extends Component {
   constructor(...args) {
     var _temp;
 
-    return _temp = super(...args), this.state = {
-      error: null
-    }, _temp;
+    return _temp = super(...args), Object.defineProperty(this, "state", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: {
+        error: null
+      }
+    }), _temp;
   }
 
   componentDidCatch(error) {
-    this.setState(() => ({ error }));
+    this.setState(() => ({
+      error
+    }));
     this.props.onCaughtError(error);
   }
+
   render() {
     return this.props.children(this.state.error);
   }
+
 }
-ErrorBoundary.propTypes = {
-  onCaughtError: PropTypes.func,
-  children: PropTypes.func
-};
-ErrorBoundary.defaultProps = {
-  onCaughtError() {},
-  children() {}
-};
+Object.defineProperty(ErrorBoundary, "propTypes", {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  value: {
+    onCaughtError: PropTypes.func,
+    children: PropTypes.func
+  }
+});
+Object.defineProperty(ErrorBoundary, "defaultProps", {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  value: {
+    onCaughtError() {},
+
+    children() {}
+
+  }
+});
